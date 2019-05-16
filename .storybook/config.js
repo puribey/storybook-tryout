c23001711fbcbf8b63b0ee32e619759e9654cd45
puribey <kenshipichu@gmail.com>
2019-05-16 13:12:52 -0400

    step 1

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+import { configure } from "@storybook/react";
+
+const req = require.context("../src", true, /.stories.tsx$/)
+function loadStories() {
+  req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module);
+
+// function loadStories() {
+//   require("../stories/index.js");
+// }
